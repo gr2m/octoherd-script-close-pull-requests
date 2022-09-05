12ef0eb0eeeb928ef34ffde606fde945ca6fdc17
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-09-05 15:14:48 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * Close pull requests across multiple repositories
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} options.author username of pull request author. Prefix app slugs with `app/`
+ */
+export async function script(octokit, repository, { author }) {}
